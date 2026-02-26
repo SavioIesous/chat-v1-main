fa96b54d52bb108b6269d914ebba5a6ba42faef0
Sávio Iesous Rocha Rodrigues <rodriguessavio68@gmail.com>
2026-02-26 19:58:50 -0300

    link

--- chat-v1-main/frontend/js/script.js
+++ chat-v1-main/frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("https://chat-v1-main.onrender.com");
     websocket.onmessage = processMessage
 
     console.log(user)
